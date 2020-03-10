b8ba814fde73777cbdf4d2562ae3bee5e296ddf3
GaryChengld <chengld@yahoo.com>
2020-03-09 22:26:46 -0400

    eslint

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,3 @@ export { default as HomePage } from './HomePage';
 export { default as MovieDetails } from './MovieDetails';
 export { default as UpcomingMovies } from './UpcomingMovies';
 export { default as NowPlayingMovies } from './NowPlayingMovies';
-
