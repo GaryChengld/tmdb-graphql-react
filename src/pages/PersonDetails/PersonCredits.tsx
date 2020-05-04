0028128210ed481f5a6a79b2c3941c918faa6bc7
GaryChengld <chengld@yahoo.com>
2020-05-03 22:25:40 -0400

    PersonCredits

---
+++ src/pages/PersonDetails/PersonCredits.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, GridList, Grid, GridListTile, Link, Typography } from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+interface PersonCreditsProps {
+  person: any;
+}
+
+export default function PersonCredits(props: PersonCreditsProps) {
+  return <></>;
+}
