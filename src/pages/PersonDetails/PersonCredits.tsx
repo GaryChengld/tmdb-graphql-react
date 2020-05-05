54ed160b52bf11c16b54cf7d71704a8cc35f65e4
GaryChengld <chengld@yahoo.com>
2020-05-04 22:05:26 -0400

    person credits

--- src/pages/PersonDetails/PersonCredits.tsx
+++ src/pages/PersonDetails/PersonCredits.tsx
@@ -6,6 +6,28 @@ interface PersonCreditsProps {
   person: any;
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    paddingLeft: theme.spacing(4),
+    width: '100%',
+    display: 'flex',
+    backgroundColor: 'transparent',
+    justifyContent: 'space-around',
+  },
+  label: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(0),
+  },
+  text: {
+    marginTop: theme.spacing(0),
+  },
+}));
+
 export default function PersonCredits(props: PersonCreditsProps) {
+  const { person } = props;
+  const classes = useStyles();
+
   return <></>;
 }
